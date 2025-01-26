2099da3676f79d3e8b6d660ed066cfd06379288c
stefeena <stefeenabenny@gmail.com>
2025-01-26 18:50:33 -0500

    Enhance Weather App: Encrypt OpenWeather API Key, Integrate Redis for Caching

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,24 +1,23 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as docker from '@pulumi/docker'
-import * as resources from '@pulumi/azure-native/resources'
-import * as containerregistry from '@pulumi/azure-native/containerregistry'
-import * as containerinstance from '@pulumi/azure-native/containerinstance'
+import * as docker from "@pulumi/docker";
+import * as resources from "@pulumi/azure-native/resources";
+import * as cache from "@pulumi/azure-native/cache";
+import * as containerregistry from "@pulumi/azure-native/containerregistry";
+import * as containerinstance from "@pulumi/azure-native/containerinstance";
 
 // Import the configuration settings for the current stack.
-const config = new pulumi.Config()
-const appPath = config.require('appPath')
-const prefixName = config.require('prefixName')
-const imageName = prefixName
-const imageTag = config.require('imageTag')
-// Azure container instances (ACI) service does not yet support port mapping
-// so, the containerPort and publicPort must be the same
-const containerPort = config.requireNumber('containerPort')
-const publicPort = config.requireNumber('publicPort')
-const cpu = config.requireNumber('cpu')
-const memory = config.requireNumber('memory')
+const config = new pulumi.Config();
+const appPath = config.require("appPath");
+const prefixName = config.require("prefixName");
+const imageName = prefixName;
+const imageTag = config.require("imageTag");
+const containerPort = config.requireNumber("containerPort");
+const publicPort = config.requireNumber("publicPort");
+const cpu = config.requireNumber("cpu");
+const memory = config.requireNumber("memory");
 
 // Create a resource group.
-const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`)
+const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`);
 
 // Create the container registry.
 const registry = new containerregistry.Registry(`${prefixName}ACR`, {
@@ -27,7 +26,8 @@ const registry = new containerregistry.Registry(`${prefixName}ACR`, {
   sku: {
     name: containerregistry.SkuName.Basic,
   },
-})
+});
+
 // Get the authentication credentials for the container registry.
 const registryCredentials = containerregistry
   .listRegistryCredentialsOutput({
@@ -38,30 +38,48 @@ const registryCredentials = containerregistry
     return {
       username: creds.username!,
       password: creds.passwords![0].value!,
-    }
-  })
- 
+    };
+  });
+
 // Define the container image for the service.
 const image = new docker.Image(`${prefixName}-image`, {
   imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
   build: {
     context: appPath,
-    platform: 'linux/amd64',
+    platform: "linux/amd64",
   },
   registry: {
     server: registry.loginServer,
     username: registryCredentials.username,
     password: registryCredentials.password,
   },
-})
+});
+
+// Create a managed Redis service
+const redis = new cache.Redis(`${prefixName}-redis`, {
+  name: `${prefixName}-weather-cache`,
+  location: "westus3",
+  resourceGroupName: resourceGroup.name,
+  enableNonSslPort: true,
+  redisVersion: "Latest",
+  minimumTlsVersion: "1.2",
+  redisConfiguration: {
+    maxmemoryPolicy: "allkeys-lru",
+  },
+  sku: {
+    name: "Basic",
+    family: "C",
+    capacity: 0,
+  },
+});
 
 // Create a container group in the Azure Container App service and make it publicly accessible.
 const containerGroup = new containerinstance.ContainerGroup(
   `${prefixName}-container-group`,
   {
     resourceGroupName: resourceGroup.name,
-    osType: 'linux',
-    restartPolicy: 'always',
+    osType: "linux",
+    restartPolicy: "always",
     imageRegistryCredentials: [
       {
         server: registry.loginServer,
@@ -76,17 +94,17 @@ const containerGroup = new containerinstance.ContainerGroup(
         ports: [
           {
             port: containerPort,
-            protocol: 'tcp',
+            protocol: "tcp",
           },
         ],
         environmentVariables: [
           {
-            name: 'PORT',
+            name: "PORT",
             value: containerPort.toString(),
           },
           {
-            name: 'WEATHER_API_KEY',
-            value: 'f5bcefdaac81320556b9103b7e72d747',
+            name: "WEATHER_API_KEY",
+            value: config.requireSecret("weatherApiKey"),
           },
         ],
         resources: {
@@ -103,16 +121,16 @@ const containerGroup = new containerinstance.ContainerGroup(
       ports: [
         {
           port: publicPort,
-          protocol: 'tcp',
+          protocol: "tcp",
         },
       ],
     },
-  },
-)
+  }
+);
 
 // Export the service's IP address, hostname, and fully-qualified URL.
-export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
-export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
+export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!);
+export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!);
 export const url = containerGroup.ipAddress.apply(
-  (addr) => `http://${addr!.fqdn!}:${containerPort}`,
-)
+  (addr) => `http://${addr!.fqdn!}:${containerPort}`
+);
