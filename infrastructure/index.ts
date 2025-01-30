960479d0e8a5387014feb84a7b3132ffb709ddef
Aakanksha <aka.pharande@gmail.com>
2025-01-29 19:51:04 -0500

    Fixed merge conflicts, deleted uncessary files and fixed directory structure

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -103,13 +103,8 @@ const containerGroup = new containerinstance.ContainerGroup(
             value: containerPort.toString(),
           },
           {
-<<<<<<< HEAD
             name: "WEATHER_API_KEY",
             value: config.requireSecret("weatherApiKey"),
-=======
-            name: 'WEATHER_API_KEY',
-            value: config.requireSecret('weatherApiKey'),
->>>>>>> 6a4084764cb5a6f7e10ea49905a5486cbb482ba8
           },
         ],
         resources: {
